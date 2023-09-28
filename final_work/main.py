2fc1f8d5623ca9fc33796176652235a816b7f01d
artempindich <artempindich@gmail.com>
2023-09-28 10:58:48 +0300

    Changed the main.py file to final_work

--- final_work/main.py
+++ final_work/main.py
@@ -1,59 +1,62 @@
+import argparse
 import requests
 from bs4 import BeautifulSoup
 import re
-from url import currency_url, population_url  # URL страниц Wiki с информацией о валютах и населении
 
 
-# Функция для очистки названия страны от лишних символов в тексте статьи на Wiki
 def clean_country_name(country_raw):
-    return re.sub(r'\[.*\.]', '', country_raw).strip()
+    return re.sub(r'\[.*\]', '', country_raw).strip()
 
 
-# Отправляем GET-запросы для получения HTML-кода страниц
-currency_response = requests.get(currency_url)
-population_response = requests.get(population_url)
+def get_country_info(country_search):
+    currency_url = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%83%D1%89%D0%B5%D1%81%D1%82%D0%B2%D1%83%D1%8E%D1%89%D0%B8%D1%85_%D0%B2%D0%B0%D0%BB%D1%8E%D1%82"
+    population_url = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%B3%D0%BE%D1%81%D1%83%D0%B4%D0%B0%D1%80%D1%81%D1%82%D0%B2_%D0%B8_%D0%B7%D0%B0%D0%B2%D0%B8%D1%81%D0%B8%D0%BC%D1%8B%D1%85_%D1%82%D0%B5%D1%80%D1%80%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%B9_%D0%BF%D0%BE_%D0%BD%D0%B0%D1%81%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D1%8E"
 
-# Создаем словарь для хранения информации о странах, валютах и населении
-country_info = {}
+    currency_response = requests.get(currency_url)
+    population_response = requests.get(population_url)
 
-# Проверяем успешность запросов
-if currency_response.status_code == 200 and population_response.status_code == 200:
-    currency_soup = BeautifulSoup(currency_response.text, 'html.parser')
-    population_soup = BeautifulSoup(population_response.text, 'html.parser')
+    country_info = {}
 
-    # Ищем таблицы с информацией о валютах
-    currency_table = currency_soup.find('table', {'class': 'wikitable'})
-    population_table = population_soup.find('table')
+    if currency_response.status_code == 200 and population_response.status_code == 200:
+        currency_soup = BeautifulSoup(currency_response.text, 'html.parser')
+        population_soup = BeautifulSoup(population_response.text, 'html.parser')
 
-    # Парсим таблицу с информацией о валютах
-    for row in currency_table.find_all('tr')[1:]:
-        cells = row.find_all('td')
-        if len(cells) >= 2:
-            country = clean_country_name(cells[0].get_text(strip=True))
-            currency = cells[1].get_text(strip=True)
-            country_info[country.lower()] = {'Валюта': currency}
+        currency_table = currency_soup.find('table', {'class': 'wikitable'})
+        population_table = population_soup.find('table')
 
-    # Парсим таблицу с информацией о населении
-    for row in population_table.find_all('tr')[1:]:
-        columns = row.find_all('td')
-        if len(columns) >= 3:
-            country_raw = columns[1].text.strip()
-            population = columns[2].text.strip()
-            country = clean_country_name(country_raw)
-            if country.lower() in country_info:
-                country_info[country.lower()]['Население'] = population
+        for row in currency_table.find_all('tr')[1:]:
+            cells = row.find_all('td')
+            if len(cells) >= 2:
+                country = clean_country_name(cells[0].get_text(strip=True))
+                currency = cells[1].get_text(strip=True)
+                country_info[country.lower()] = {'Валюта': currency}
 
-    # Запрашиваем у пользователя название страны и приводим его к нижнему регистру
-    country_search = input("Введите название страны, чтобы получить информацию: ").lower()
+        for row in population_table.find_all('tr')[1:]:
+            columns = row.find_all('td')
+            if len(columns) >= 3:
+                country_raw = columns[1].text.strip()
+                population = columns[2].text.strip()
+                country = clean_country_name(country_raw)
+                if country.lower() in country_info:
+                    country_info[country.lower()]['Население'] = population
 
-    # Выводим информацию о стране, валюте и населении
-    if country_search in country_info:
-        info = country_info[country_search]
-        country_name = country_search.capitalize()  # Преобразуем первую букву в верхний регистр
-        print(f"Страна: {country_name}")
-        print(f"Валюта: {info.get('Валюта', 'Информация отсутствует')}")
-        print(f"Население: {info.get('Население', 'Информация отсутствует')}")
+        country_search = country_search.lower()
+
+        if country_search in country_info:
+            info = country_info[country_search]
+            country_name = country_search.capitalize()  # Преобразуем первую букву в верхний регистр
+            print(f"Страна: {country_name}")
+            print(f"Валюта: {info.get('Валюта', 'Информация отсутствует')}")
+            print(f"Население: {info.get('Население', 'Информация отсутствует')}")
+        else:
+            print("Информация о стране не найдена.")
     else:
-        print("Информация о стране не найдена.")
-else:
-    print("Ошибка при получении страниц")
+        print("Ошибка при получении страниц")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Получение информации о стране")
+    parser.add_argument("country_search", type=str, help="Название страны для поиска информации")
+
+    args = parser.parse_args()
+    get_country_info(args.country_search)
